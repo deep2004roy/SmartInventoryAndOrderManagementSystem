6b41af5fefdce2f871b06027153ff2d8141d741d
Deep <droy00524@gmail.com>
2026-05-10 00:29:33 +0530

    added other folders

---
+++ src/main/java/com/deep/smartinventoryandordermanagementsystem/SmartInventoryAndOrderManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.deep.smartinventoryandordermanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartInventoryAndOrderManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartInventoryAndOrderManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deep/smartinventoryandordermanagementsystem/SmartInventoryAndOrderManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deep.smartinventoryandordermanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartInventoryAndOrderManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
